c3ea99b987217bd98461e39ed05fb0198b549057
Necdet Yavuz AKTAS <aktasnecdetyavuz@gmail.com>
2022-05-14 21:32:00 +0300

    Last line of code is written

---
+++ ToH.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void hanoiTower( int n, int from, int to, int aux );
+
+
+
+int main( int from, int to, int aux ){
+
+
+	int  number;	//The number of disks to be moved
+
+	
+
+
+	printf("Enter the number of disks : ");
+	scanf("%d", &number );
+
+	hanoiTower( number, 1, 2, 3 );
+	
+
+
+}
+
+
+
+
+void hanoiTower( int n, int from, int  aux, int to ){
+
+
+
+	if( n == 1 ){
+
+		printf("%d --> %d\n", from, to );
+
+	}
+
+
+	else{
+
+		hanoiTower( n - 1, from, to, aux );	//The peg on which these disks are initially threaded. The peg to which this stack of disks is to be moved. The peg to be used as a temporary holding area
+		printf("%d --> %d\n", from, to );
+		hanoiTower( n - 1, aux, from, to );
+	}
+	
+
+}
